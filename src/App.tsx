d4097a800cda1a67522e373c8237830e2d25d26e
Brandon Lim <limws.brandon@gmail.com>
2021-06-25 23:00:27 +0800

    Improve navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import MainContainer from "./components/maincontainer/MainContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
 
 function App() {
   return (
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,14 @@
 import { Navbar, Nav } from "react-bootstrap";
 
 const Header = () => (
-  <Navbar bg="primary" variant="dark" expand="lg" >
-    <Navbar.Brand href="/">Smart Gardening</Navbar.Brand>
+  <Navbar bg="primary" variant="dark" expand="lg">
+    <Navbar.Brand href="/">{" "}Smart Gardening</Navbar.Brand>
+    <Navbar.Toggle></Navbar.Toggle>
     <Navbar.Collapse>
       <Nav className="mr-auto">
         <Nav.Link href="/">Home</Nav.Link>
       </Nav>
-    </Navbar.Collapse>
+    </Navbar.Collapse>    
   </Navbar>
 );
 
